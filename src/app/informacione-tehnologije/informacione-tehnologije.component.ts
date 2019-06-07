046505b12c324794cde0cb176c5272ef6e74e850
Andrija995 <50317500+Andrija995@users.noreply.github.com>
2019-06-07 11:12:57 +0200

    informacione-admin

--- src/app/informacione-tehnologije/informacione-tehnologije.component.ts
+++ src/app/informacione-tehnologije/informacione-tehnologije.component.ts
@@ -4,6 +4,7 @@ import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
 import { LoginAuthService } from '../login-auth.service';
+import { AuthGuard } from 'auth.guard';
 
 @Component({
   selector: 'app-informacione-tehnologije',
@@ -11,63 +12,69 @@ import { LoginAuthService } from '../login-auth.service';
   styleUrls: ['./informacione-tehnologije.component.css']
 })
 export class InformacioneTehnologijeComponent implements OnInit {
+  showActions
+  public objekat: any = [];
+  closeResult: string;
 
-    public objekat: any = [];
-    closeResult: string;
-
-      public id //id za modal i delete button, ubacujem ga u deleteRow funkciju i vezan je za id_course
+  public id //id za modal i delete button, ubacujem ga u deleteRow funkciju i vezan je za id_course
   loginuser: any;
-  constructor(private authService: LoginAuthService, private subject: UserService, private router: Router, private toastr: ToastrService, private modalService: NgbModal) { 
+  authorizationService: any;
+  constructor(private authService: LoginAuthService, private subject: UserService, private router: Router, private toastr: ToastrService, private modalService: NgbModal) {
     this.authService.isLoggedIn();
     this.loginuser = JSON.parse(localStorage.getItem('currentUser'));
   }
 
-  
+
 
   ngOnInit() {
 
+    this.showActions = JSON.parse(localStorage.getItem('currentUser')).user.role
+    console.log(this.showActions)
+
     this.getInsert()
   }
 
-  getInsert(){
+  getInsert() {
 
-    this.subject.getInsert().subscribe(res => this.objekat=res, err => console.log(err))
-    
+    this.subject.getInsert().subscribe(res => this.objekat = res, err => console.log(err))
+
   }
- 
- deleteRow(){
-  this.subject.deleteRow(this.id).subscribe(res => {
-    console.log(res) 
-    this.getInsert()
-    
+
+  deleteRow() {
+    this.subject.deleteRow(this.id).subscribe(res => {
+      console.log(res)
+      this.getInsert()
+
+    }
+
+    )
+
   }
-    
-  )
-   
- }
 
-updateRow(id){
-  this.router.navigate(['informacione-update', id])
-}
-
-open(content, id) {
-  this.id=id;
-  this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
-    this.closeResult = `Closed with: ${result}`;
-  }, (reason) => {
-    this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
-  });
-}
-
-private getDismissReason(reason: any): string {
-  if (reason === ModalDismissReasons.ESC) {
-    return 'by pressing ESC';
-  } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
-    return 'by clicking on a backdrop';
-  } else {
-    return  `with: ${reason}`;
+  updateRow(id) {
+    this.router.navigate(['informacione-update', id])
   }
-}
+
+  open(content, id) {
+    this.id = id;
+    this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title' }).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return `with: ${reason}`;
+    }
+  }
+
+
 
 
 }
