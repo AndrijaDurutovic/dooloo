0a8467e2b18c930ede3cc11ede2b1ec640e351e8
Andrija995 <50317500+Andrija995@users.noreply.github.com>
2019-06-17 15:20:27 +0200

    upload-img

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { UserService } from '../user.service';
 import { LoginAuthService } from '../login-auth.service';
 import { ToastrService } from 'ngx-toastr';
+import { HttpClient } from '@angular/common/http';
 
 
 
@@ -12,17 +13,26 @@ import { ToastrService } from 'ngx-toastr';
 })
 export class SignupComponent implements OnInit {
 
+  selectedFile: File 
+  public slika: any 
 
-  public user: any = {};
+  public user: any = {
+    firstName: null,
+    lastName: null,
+    email:null,
+    phone:null,
+    role:null,
+  profilePicPath: null
+  };
 
-  constructor(private userService: UserService, private authService: LoginAuthService, private toastr: ToastrService) { 
+  constructor(private http: HttpClient,private userService: UserService, private authService: LoginAuthService, private toastr: ToastrService) { 
     this.authService.isLoggedIn();
   }
 
   ngOnInit() {
   }
-  saveUser(user:any, userForm: any){
-    
+  saveUser(user:any, userForm: any,){
+   
     user.enabled = true;
     this.userService.saveUser(user).subscribe((response) => {
       if(response){
@@ -39,6 +49,23 @@ export class SignupComponent implements OnInit {
    
     )
   }
+  onFileSelected(event){
+    console.log(event);
+    this.selectedFile=<File>event.target.files[0];
+  }
+
+  onUpload(){
+    const fd = new FormData();
+    fd.append('image', this.selectedFile, this.selectedFile.name);
+    this.http.post('https://us-central1-imageupload-2fc33.cloudfunctions.net/uploadFile', fd )
+    .subscribe(res=>{
+      console.log('Url slike:' ,res);
+      this.user.profilePicPath=res
+      this.user.profilePicPath=this.user.profilePicPath.message
+
+    });
+  }
+
 
 
 }
--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -18,73 +18,73 @@ export class UserService {
   }
   saveUser(user: any): Observable<any> {
     const headers = new HttpHeaders({ 'Access-Control-Allow-Origin': '*' });
-    return this.http.post("https://doolooapp.herokuapp.com/registration", user);
+    return this.http.post("http://192.168.0.9:8080/registration", user);
 
   }
   loginUser(user: any): Observable<any> {
     const headers = new HttpHeaders({ 'Access-Control-Allow-Origin': '*' });
-    return this.http.post("https://doolooapp.herokuapp.com/login", user);
+    return this.http.post("http://192.168.0.9:8080/login", user);
 
   }
   getAllUsers(token: any): Observable<any> {
     const headers = new HttpHeaders({ 'Authorization': 'Bearer ' + token });
-    return this.http.get("https://doolooapp.herokuapp.com/users", { headers: headers });
+    return this.http.get("http://192.168.0.9:8080/users", { headers: headers });
 
   }
 
   getUser(token: any, ): Observable<any> {
     const headers = new HttpHeaders({ 'Authorization': 'Bearer ' + token });
-    return this.http.get("https://doolooapp.herokuapp.com/getuser" , { headers: headers });
+    return this.http.get("http://192.168.0.9:8080/getuser" , { headers: headers });
 
   }
 
   deleteUser(user: any) {
     const headers = new HttpHeaders ({'Access-Control-Allow-Origin': '*'});
-    return this.http.delete("https://doolooapp.herokuapp.com/users/" +user);
+    return this.http.delete("http://192.168.0.9:8080/users/" +user);
    
   }
   updateUser(id, user) {
     const headers = new HttpHeaders ({'Access-Control-Allow-Origin': '*'});
-    return this.http.put("https://doolooapp.herokuapp.com/users/" +id, user);
+    return this.http.put("http://192.168.0.9:8080/users/" +id, user);
    
   }
   getUserById(token, id): Observable<any> {
     const headers = new HttpHeaders({ 'Authorization': 'Bearer ' + token });
-    return this.http.get("https://doolooapp.herokuapp.com/users/" +id , { headers: headers });
+    return this.http.get("http://192.168.0.9:8080/users/" +id , { headers: headers });
 
   }
 
   insert(subject): Observable<any> {
     const headers = new HttpHeaders({ 'Authorization': 'Bearer ' });
-    return this.http.post("https://doolooapp.herokuapp.com/courses/", subject );
+    return this.http.post("http://192.168.0.9:8080/courses/", subject );
   }
   getInsert(): Observable<any> {
     const headers = new HttpHeaders({ 'Authorization': 'Bearer ' });
-    return this.http.get("https://doolooapp.herokuapp.com/courses/");
+    return this.http.get("http://192.168.0.9:8080/courses/");
 }
 
 deleteRow(id_course): Observable<any> {
   const headers = new HttpHeaders({ 'Authorization': 'Bearer ' });
-  return this.http.delete("https://doolooapp.herokuapp.com/courses/" +id_course);
+  return this.http.delete("http://192.168.0.9:8080/courses/" +id_course);
 
 }
 updateRow(id_course, subjects): Observable<any> {
   const headers = new HttpHeaders({ 'Authorization': 'Bearer ' });
-  return this.http.put("https://doolooapp.herokuapp.com/courses/" +id_course, subjects, {headers: headers});
+  return this.http.put("http://192.168.0.9:8080/courses/" +id_course, subjects, {headers: headers});
 
 }
 getUpdateById(id_course): Observable<any> {
   const headers = new HttpHeaders({ 'Authorization': 'Bearer ' });
-  return this.http.get("https://doolooapp.herokuapp.com/courses/" + id_course );
+  return this.http.get("http://192.168.0.9:8080/courses/" + id_course );
 }
 contact(contacts): Observable<any> {
   const headers = new HttpHeaders({ 'Access-Control-Allow-Origin': '*' });
-  return this.http.post("https://doolooapp.herokuapp.com/contacts", contacts);
+  return this.http.post("http://192.168.0.9:8080/contacts", contacts);
 }
 
 getAllContacts(contacts): Observable<any> {
   const headers = new HttpHeaders({ 'Authorization': 'Bearer ' });
-  return this.http.get("https://doolooapp.herokuapp.com/contacts/", contacts);
+  return this.http.get("http://192.168.0.9:8080/contacts/", contacts);
 
 }
 
